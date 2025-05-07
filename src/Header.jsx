402aa19e6b3a90581b8a7581f7e7c02f3818f125
monishamuthuraman2 <monisha.muthuraman@hcltech.com>
2025-05-07 22:05:23 +0530

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+export default function Header(){
+    return(
+        <header>
+        <img src="src/assets/globe.svg" alt="globe-icon" className="logo"></img>
+        <span className="logo-text">travel journey</span>
+        </header>
+    )
+}
